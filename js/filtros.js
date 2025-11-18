e991e1b4e1683de94fdb457c5e7571b7ab90cdc9
Andres Serna <anferi.00@gmail.com>
2025-11-18 18:33:43 -0500

    se modifican los files js/filtros.js y Ferreteria.html y se agrega el file react.html

--- js/filtros.js
+++ js/filtros.js
@@ -1,49 +1,13 @@
-//// Seleccionamos todos los filtros (los tres <select>)
-//const filtros = document.querySelectorAll('.btn-filtro');
-//const mensaje = document.querySelector('#filter-selected-value');
-//
-//
-//
-//
-//// Recorremos cada filtro para escuchar los cambios
-//filtros.forEach(filtro => {
-//  filtro.addEventListener('change', function () {
-//
-//    const productos = document.querySelectorAll('.articulo');
-//
-//    const selectedValue = filtro.value;
-//
-//    // Mostrar el valor seleccionado
-//    if (selectedValue) {
-//      mensaje.textContent = `Has seleccionado: ${selectedValue}`;
-//    } else {
-//      mensaje.textContent = '';
-//    }
-//
-//    // Filtrar productos según la modalidad (atributo data-modalidad)
-//    productos.forEach(insumo => {
-//      const modalidad = insumo.dataset.modalidad;
-//
-//      if (selectedValue === '' || modalidad === selectedValue) {
-//        insumo.style.display = 'block';
-//      } else {
-//        insumo.style.display = 'none';
-//      }
-//    });
-//  });
-//});
 
-
-// -----------------------------------------------------------
 // 1. Seleccionar los filtros y el mensaje de texto
-// -----------------------------------------------------------
+
 const filtros = document.querySelectorAll('.btn-filtro');
 const mensaje = document.querySelector('#filter-selected-value');
 
 
-// -----------------------------------------------------------
+
 // 2. Función que aplica el filtrado a las SECCIONES
-// -----------------------------------------------------------
+
 function aplicarFiltro(valor) {
 
     const secciones = document.querySelectorAll('.articulo');
@@ -60,26 +24,26 @@ function aplicarFiltro(valor) {
 }
 
 
-// -----------------------------------------------------------
+
 // 3. Activar filtro cuando cambia un <select>
-// -----------------------------------------------------------
+
 filtros.forEach(filtro => {
     filtro.addEventListener('change', function () {
 
         const selectedValue = filtro.value.trim().toLowerCase();
 
-        mensaje.textContent = selectedValue
-            ? `Has seleccionado: ${selectedValue}`
-            : "";
+        //mensaje.textContent = selectedValue
+        //    ? `Has seleccionado: ${selectedValue}`
+        //    : "";
 
         aplicarFiltro(selectedValue);
     });
 });
 
 
-// -----------------------------------------------------------
+
 // 4. Activar filtro cuando se hace clic en el navbar
-// -----------------------------------------------------------
+
 const enlacesNav = document.querySelectorAll('a.nav-link, a.dropdown-item');
 
 enlacesNav.forEach(link => {
